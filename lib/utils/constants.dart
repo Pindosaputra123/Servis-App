406ab0956a3f4fd5c98e0cbf4f6cfc29dbbcd33d
Pindosaputra123 <pindoajja864@gmail.com>
2025-10-09 12:18:39 +0700

    Primary Color

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
 class Constants {
-  static final Color primaryColor = Color.fromRGBO(18, 26, 28, 1);
-  static final Color greyColor = Color.fromRGBO(247, 247, 249, 1);
+  static final Color primaryColor = Color(0xFF02569B);
+  static final Color greyColor = Color(0xFFF5F5F5);
 }
